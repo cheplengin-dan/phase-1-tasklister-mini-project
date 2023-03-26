706df7bc40a525607225314be5a854fb337cf0d2
Duncan <cheplenginduncan@gmail.com>
2023-03-26 21:31:20 +0300

    Completed Challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const tasksList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+    deleteButton.addEventListener("click", () => {
+      newTask.remove();
+    });
+
+    newTask.appendChild(deleteButton);
+
+    tasksList.appendChild(newTask);
+
+    taskInput.value = "";
+  });
 });
